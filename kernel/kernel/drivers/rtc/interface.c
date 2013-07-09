fa0a61cfecbcb1d0789220926d3d3838f39cbc06
OmarBizreh <omarbizrehxda@gmail.com>
2013-07-09 16:34:48 +0300

    Update 3.0.26 -> 3.0.27

--- kernel/kernel/drivers/rtc/interface.c
+++ kernel/kernel/drivers/rtc/interface.c
@@ -790,6 +790,14 @@ static void rtc_alarm_disable(struct rtc_device *rtc)
 	___rtc_set_alarm(rtc, &alarm);
 }
 
+static void rtc_alarm_disable(struct rtc_device *rtc)
+{
+	if (!rtc->ops || !rtc->ops->alarm_irq_enable)
+		return;
+
+	rtc->ops->alarm_irq_enable(rtc->dev.parent, false);
+}
+
 /**
  * rtc_timer_remove - Removes a rtc_timer from the rtc_device timerqueue
  * @rtc rtc device
@@ -811,8 +819,10 @@ static void rtc_timer_remove(struct rtc_device *rtc, struct rtc_timer *timer)
 		struct rtc_wkalrm alarm;
 		int err;
 		next = timerqueue_getnext(&rtc->timerqueue);
-		if (!next)
+		if (!next) {
+			rtc_alarm_disable(rtc);
 			return;
+		}
 		alarm.time = rtc_ktime_to_tm(next->expires);
 		alarm.enabled = 1;
 		err = __rtc_set_alarm(rtc, &alarm);
@@ -874,7 +884,8 @@ again:
 		err = __rtc_set_alarm(rtc, &alarm);
 		if (err == -ETIME)
 			goto again;
-	}
+	} else
+		rtc_alarm_disable(rtc);
 
 	mutex_unlock(&rtc->ops_lock);
 }
