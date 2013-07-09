0ddfdcbae1ab8b0fe2bd4c5a76c62f08549aa939
OmarBizreh <omarbizrehxda@gmail.com>
2013-07-09 16:23:12 +0300

    Update 3.0.18 -> 3.0.19

--- kernel/kernel/drivers/tty/serial/amba-pl011.c
+++ kernel/kernel/drivers/tty/serial/amba-pl011.c
@@ -2046,6 +2046,10 @@ pl011_console_write(struct console *co, const char *s, unsigned int count)
 		spin_unlock(&uap->port.lock);
 	local_irq_restore(flags);
 
+	if (locked)
+		spin_unlock(&uap->port.lock);
+	local_irq_restore(flags);
+
 	clk_disable(uap->clk);
 }
 
