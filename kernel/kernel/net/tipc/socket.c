6aaa052933744f1552f60fc5c0a58bbdb41d3e6b
OmarBizreh <omarbizrehxda@gmail.com>
2013-07-10 17:18:20 +0300

    Update 3.0.75 -> 3.0.76

--- kernel/kernel/net/tipc/socket.c
+++ kernel/kernel/net/tipc/socket.c
@@ -952,6 +952,9 @@ static int recv_msg(struct kiocb *iocb, struct socket *sock,
 	/* will be updated in set_orig_addr() if needed */
 	m->msg_namelen = 0;
 
+	/* will be updated in set_orig_addr() if needed */
+	m->msg_namelen = 0;
+
 	timeout = sock_rcvtimeo(sk, flags & MSG_DONTWAIT);
 restart:
 
@@ -1077,6 +1080,9 @@ static int recv_stream(struct kiocb *iocb, struct socket *sock,
 		res = -ENOTCONN;
 		goto exit;
 	}
+
+	/* will be updated in set_orig_addr() if needed */
+	m->msg_namelen = 0;
 
 	/* will be updated in set_orig_addr() if needed */
 	m->msg_namelen = 0;
